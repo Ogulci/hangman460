51584cc714fedb25828a9a4e51cac9813ff740ef
Ogulcan Ozturk <oglozt@gmail.com>
2024-02-03 14:55:45 +0000

    while loop has been deleted for milestone_2

--- milestone_2.py
+++ milestone_2.py
@@ -7,11 +7,11 @@ print(word_list)
 word= random.choice(word_list)
 print(word)
 
-while True:
-    guess= input("Please enter a single letter:   ")
 
-    if len(guess)==1 and guess.isalpha():
-        print("Good guess!")
-        break
-    else:
-        print("Oops! That is not a valid input.") 
+guess= input("Please enter a single letter:   ")
+
+if len(guess)==1 and guess.isalpha():
+    print("Good guess!")
+        
+else:
+    print("Oops! That is not a valid input.") 
